28066b50ce8232cba3a710ab0c4eb304416660cd
Sergey Petrunin <halfstrik@gmail.com>
2025-07-27 12:17:55 -0400

    Initial attempt, I think I messed up columns and rows

--- keyboards/handwired/cardboard_advantage/keymaps/default/keymap.c
+++ keyboards/handwired/cardboard_advantage/keymaps/default/keymap.c
@@ -4,21 +4,12 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
     [0] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+        KC_5, KC_0,
+        KC_4, KC_9,
+        KC_3, KC_8,
+        KC_2, KC_7,
+        KC_1, KC_6,
+        KC_0, KC_DOT
     )
 };
