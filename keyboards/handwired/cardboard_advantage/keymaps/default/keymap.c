222806f4ae2df091e5ee6fa4fc9755023afda723
Sergey Petrunin <halfstrik@gmail.com>
2026-02-19 18:43:12 -0500

    Set for new black USB-C keyboard

--- keyboards/handwired/cardboard_advantage/keymaps/default/keymap.c
+++ keyboards/handwired/cardboard_advantage/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS,  KC_A,   KC_S,     KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,   KC_X,     KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
         KC_L,     KC_GRV, KC_ESC,   KC_LEFT,  KC_RIGHT,KC_A,    KC_B,    KC_UP,   KC_DOWN, KC_LBRC,  KC_RBRC, KC_R,
-        KC_LCTL,  KC_SPC, MO(_FN),  KC_LCMD,  KC_LALT ,KC_ESC,  KC_DEL,  KC_RALT, KC_RCMD, KC_RCTL,  KC_BSPC,  KC_ENTER
+        KC_ESC,   KC_LALT,KC_LCMD,  MO(_FN),  KC_SPC,  KC_LCTL, KC_ENTER,KC_BSPC, KC_RCTL, KC_RCMD,  KC_RALT, KC_DEL
     ),
     [_FN] = LAYOUT(
         KC_ESC,   KC_F1,  KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
